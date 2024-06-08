31e32a8440f0fc7f4a58f27faf40061f8e00bad5
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2024-06-08 03:05:48 -0400

    Test for root()
    Updated MathTest.java to test power and root functions of MyMath class

--- MathTest.java
+++ MathTest.java
@@ -1,5 +1,7 @@
 public class MathTest {
     public static void main(String[] args) {
-        System.out.println(MyMath.PI_CONSTANT / 180);
+        System.out.println(MyMath.power(10,-10));
+        System.out.println(MyMath.root(5.0625,4));
+
     }
 }
