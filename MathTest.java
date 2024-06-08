49bc493a812f7d855b7f0a08056451a38fb6ff52
cdwilkie <80400795+cdwilkie@users.noreply.github.com>
2024-06-08 04:11:07 -0400

    Added gcd() test
    Updated MathTest.java to test new gcd() method.

--- MathTest.java
+++ MathTest.java
@@ -1,7 +1,8 @@
 public class MathTest {
     public static void main(String[] args) {
         System.out.println(MyMath.power(10,-10));
-        System.out.println(MyMath.root(5.0625,4));
+        System.out.println(MyMath.root(599,2));
+        System.out.println(MyMath.gcd(24,18));
 
     }
 }
