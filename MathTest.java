6c03e4a752afed3d54f0015471474b08aef26cf5
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-06-09 01:48:58 -0400

    Added test for lcm()
    Updated MathTest.java to test the lcm() method

--- MathTest.java
+++ MathTest.java
@@ -2,6 +2,7 @@ public class MathTest {
     public static void main(String[] args) {
         System.out.println(MyMath.toDegrees(MyMath.PI_CONSTANT));
         System.out.println(MyMath.power(13, 2));
+        System.out.println(MyMath.lcm(4,6));
 
     }
 }
