bdabdca9bbcb36c0a6ec6f25a1c529a4faaeebd3
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-06-09 17:13:35 -0400

    Added sine() test
    Updated MathTest.java to include a test for sine() function

--- MathTest.java
+++ MathTest.java
@@ -3,6 +3,7 @@ public class MathTest {
         System.out.println(MyMath.toDegrees(MyMath.PI_CONSTANT));
         System.out.println(MyMath.power(13, 2));
         System.out.println(MyMath.lcm(4,6));
+        System.out.println(MyMath.sine(1.047197551));
 
     }
 }
