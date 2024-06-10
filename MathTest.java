92fe5311ceeba127476d9705cc90de4a571534a6
cdwilkie <cdwilkie@aggies.ncat.edu>
2024-06-09 21:29:21 -0400

    Added tests for root()
    Updated MathTest.java to include java.lang.Math calls to check accuracy of MyMath methods.

--- MathTest.java
+++ MathTest.java
@@ -3,8 +3,10 @@ public class MathTest {
     public static void main(String [] args) {
         System.out.println("Testing root root(35, 5)");
         System.out.println(MyMath.root(35,5));
-        System.out.println("Testing root root(16.5, 2)");
+        System.out.println(Math.pow(35, 0.2));
+        System.out.println("\nTesting root root(16.5, 2)");
         System.out.println(MyMath.root(16.5,2));
+        System.out.println(Math.pow(16.5, 0.5));
 
     }
 }    
